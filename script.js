bd2e016ab4f397ee793143072b05a9bf0d34c2dd
MikeMartison <88384399+DolorPaid@users.noreply.github.com>
2026-01-22 01:22:15 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Серверная часть
 const API_URL = 'https://dolorpaid-github-io.onrender.com';
 
 async function getData() {
@@ -11,17 +12,20 @@ async function getData() {
     }
 }
 
+
+// Клиентская часть
 const body = document.querySelector('body')
+
 
 function StartScreen() {
     const hello_world = document.createElement('div')
     hello_world.style = `
-                display: flex;
-                opacity: 0;
-                height: 90vh;
-                font-size: 75px;
-                justify-content: center;
-                align-items: center;
+    display: flex;
+    opacity: 0;
+    height: 90vh;
+    font-size: 75px;
+    justify-content: center;
+    align-items: center;
     `
 
     hello_world.innerHTML = 'Hello, World!'
@@ -30,14 +34,49 @@ function StartScreen() {
 
     setTimeout(() => {
         hello_world.style.opacity = 1
+        setTimeout(() => {
+            hello_world.style.opacity = 0
+            setTimeout(() => {
+                hello_world.remove()
+                StartMain()
+            }, 500);
+        }, 1000);
     }, 500);
+}
+
+
+function StartMain() {
+    const cont = document.querySelector('#container')
+
+    const Cells = [
+        ['Discord', "Сообщество Автора", 'https://discord.gg/DSWQjTq'],
+        ['Сайт App8ook', "Сайт-Сборник программ, ссылок и информации [by @Ko5ou]", 'https://app8ook.github.io'],
+    ]
+
+    Cells.forEach(Cell => {
+        const cell = document.createElement('cell')
+        cell.classList.add('cell')
+        cell.onclick = `GoTo(${Cell[2]})`
+        const Title = document.createElement('h2')
+        Title.innerHTML = Cell[0]
+        const Desc = document.createElement('desc')
+        Desc.innerHTML = Cell[1]
+
+        cell.append(Title, Desc)
+        cont.append(cell)
+    })
+
+    cont.style.display = 'flex'
+    setTimeout(() => {
+        cont.style.opacity = 1
+    }, 10);
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
     StartScreen()
-
-    setTimeout(() => {
-        getData();
-    }, 3000);
 })
 
+
+function GoTo(link){
+    window.open(link, '_blank')
+}
