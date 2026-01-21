6a160b7786534f4b7e9b27be1ba2dc0f1cb66084
MikeMartison <88384399+DolorPaid@users.noreply.github.com>
2026-01-22 01:06:09 +0400

    Update script.js
    Add API-URL

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-const API_URL = 'httpshttps://dolorpaid-github-io.onrender.com';
+const API_URL = 'https://dolorpaid-github-io.onrender.com';
 
 async function getData() {
-  try {
-    const response = await fetch(`${API_URL}/api/data`);
-    const data = await response.json();
-    console.log(data);
-    // ... обновите интерфейс
-  } catch (error) {
-    console.error('Ошибка при запросе к API:', error);
-  }
+    try {
+        const response = await fetch(`${API_URL}/api/data`);
+        const data = await response.json();
+        console.log(data);
+        // ... обновите интерфейс
+    } catch (error) {
+        console.error('Ошибка при запросе к API:', error);
+    }
 }
 
-// Вызовите функцию при загрузке страницы
-getData();
+const body = document.querySelector('body')
+
+function StartScreen() {
+    const hello_world = document.createElement('div')
+    hello_world.style = `
+                display: flex;
+                opacity: 0;
+                height: 90vh;
+                font-size: 75px;
+                justify-content: center;
+                align-items: center;
+    `
+
+    hello_world.innerHTML = 'Hello, World!'
+
+    body.prepend(hello_world)
+
+    setTimeout(() => {
+        hello_world.style.opacity = 1
+    }, 500);
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    StartScreen()
+
+    setTimeout(() => {
+        getData();
+    }, 3000);
+})
+
